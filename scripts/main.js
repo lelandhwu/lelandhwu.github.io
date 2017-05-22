f3ba7fc78da12931f8ebf99e2521ab2c2399b4f4
Leland Wu <leland.wu4@gmail.com>
2017-05-21 23:50:53 -0400

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+
+        var mySrc = myImage.getAttribute('src');
+
+        if(mySrc === 'images/portal-boy.png') {
+                myImage.setAttribute ('src','images/2B.png');
+        } 
+        else {
+                myImage.setAttribute ('src','images/portal-boy.png');
+        }
+
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Hi, ' + myName + '!';
+
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} 
+else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Hi,' + storedName + '!';
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+
